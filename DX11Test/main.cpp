2217e3a4ac93749db7f6d835611c9bfaf15e51df
D761454 <144056074+D761454@users.noreply.github.com>
2024-09-19 11:03:20 +0100

    make all

---
+++ DX11Test/main.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+#include "Window.h"
+
+int CALLBACK WinMain(HINSTANCE appInstance, HINSTANCE prevInstance, LPSTR cmdLine, int cmdCount) {
+
+	Window window(800, 600);
+
+	return 0;
+}
