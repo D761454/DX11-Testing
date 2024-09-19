aacebc8304fd8c9c018bf9e165bc64e00bc02423
D761454 <144056074+D761454@users.noreply.github.com>
2024-09-19 11:20:26 +0100

    responsive window

--- DX11Test/Window.cpp
+++ DX11Test/Window.cpp
@@ -1,11 +1,22 @@
 #include <Windows.h>
 #include "Window.h"
 
-Window::Window(int width, int height) {
+LRESULT CALLBACK WinProc(HWND handle, UINT msg, WPARAM wparam, LPARAM lparam) {
 
+	return DefWindowProc(handle, msg, wparam, lparam);
+}
+
+Window::Window(int width, int height) {
+	// define windows style
+	WNDCLASS wc = { 0 };
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WinProc;
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.lpszClassName = "DX11Test";
+	RegisterClass(&wc);
 
 	// create window
-	CreateWindow("", "DX11Test", 
+	CreateWindow("DX11Test", "DX11Test before uni", 
 		WS_POPUP | WS_CAPTION | WS_SYSMENU | WS_VISIBLE, 
 		100, 100, width, height, 
 		nullptr, nullptr, nullptr, nullptr);
--- DX11Test/main.cpp
+++ DX11Test/main.cpp
@@ -5,5 +5,13 @@ int CALLBACK WinMain(HINSTANCE appInstance, HINSTANCE prevInstance, LPSTR cmdLin
 
 	Window window(800, 600);
 
+	MSG msg = { 0 };
+	while (true) {
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
 	return 0;
 }
