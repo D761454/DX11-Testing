24fe3bfde661ae275c5dbe4981b9634da1ef0625
D761454 <144056074+D761454@users.noreply.github.com>
2024-09-20 14:51:14 +0100

    creating and loading shaders

--- DX11Test/Triangle.cpp
+++ DX11Test/Triangle.cpp
@@ -1,4 +1,7 @@
 #include "Triangle.h"
+#include <fstream>
+#include <vector>
+using namespace std;
 
 struct Vertex {
 	float x, y;
@@ -6,6 +9,31 @@ struct Vertex {
 };
 
 Triangle::Triangle(Renderer& renderer) {
+	createMesh(renderer);
+	createShaders(renderer);
+}
+
+Triangle::~Triangle() {
+	m_vertexBuffer->Release();
+	m_vertexShader->Release();
+	m_pixelShader->Release();
+}
+
+void Triangle::draw(Renderer& renderer) {
+	// bind triangle shaders
+	renderer.getDeviceContext()->VSSetShader(m_vertexShader, nullptr, 0);
+	renderer.getDeviceContext()->PSSetShader(m_pixelShader, nullptr, 0);
+
+	// bind vertex buffer
+	UINT stride = sizeof(Vertex);
+	UINT offset = 0;
+	renderer.getDeviceContext()->IASetVertexBuffers(0, 1, &m_vertexBuffer, &stride, &offset);
+
+	// draw
+	renderer.getDeviceContext()->Draw(3, 0);
+}
+
+void Triangle::createMesh(Renderer& renderer) {
 	// define vertices
 	Vertex vertices[] = {
 		{-1, -1, 1, 0, 0},
@@ -21,12 +49,13 @@ Triangle::Triangle(Renderer& renderer) {
 	renderer.getDevice()->CreateBuffer(&vertexBufferDesc, &vertexData, &m_vertexBuffer);
 }
 
-void Triangle::draw(Renderer& renderer) {
-	// bind vertex buffer
-	UINT stride = sizeof(Vertex);
-	UINT offset = 0;
-	renderer.getDeviceContext()->IASetVertexBuffers(0, 1, &m_vertexBuffer, &stride, &offset);
+void Triangle::createShaders(Renderer& renderer) {
+	ifstream vsFile("triangleVertexShader.cso", ios::binary);
+	ifstream psFile("trianglePixelShader.cso", ios::binary);
 
-	// draw
-	renderer.getDeviceContext()->Draw(3, 0);
+	vector<char> vsData = { istreambuf_iterator<char>(vsFile), istreambuf_iterator<char>() };
+	vector<char> psData = { istreambuf_iterator<char>(psFile), istreambuf_iterator<char>() };
+
+	renderer.getDevice()->CreateVertexShader(vsData.data(), vsData.size(), nullptr, &m_vertexShader);
+	renderer.getDevice()->CreatePixelShader(psData.data(), psData.size(), nullptr, &m_pixelShader);
 }
--- DX11Test/Triangle.h
+++ DX11Test/Triangle.h
@@ -5,8 +5,14 @@ class Triangle
 {
 public:
 	Triangle(Renderer& renderer);
+	~Triangle();
 	void draw(Renderer& renderer);
 private:
+	void createMesh(Renderer& renderer);
+	void createShaders(Renderer& renderer);
+
 	ID3D11Buffer* m_vertexBuffer = nullptr;
+	ID3D11VertexShader* m_vertexShader = nullptr;
+	ID3D11PixelShader* m_pixelShader = nullptr;
 };
 
